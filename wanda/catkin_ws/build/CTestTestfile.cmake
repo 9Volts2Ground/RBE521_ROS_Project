7f0ab3902a1d9cb2d630636e9e984832dfd3f7ab
9Volts2Ground <s.adam.stringham@gmail.com>
2021-07-01 22:14:31 -0700

    Make first catkin_ws. Started a notes file to keep track of things I learn about ROS

---
+++ wanda/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vision/project/RBE521_ROS_Project/wanda/catkin_ws/src
+# Build directory: /home/vision/project/RBE521_ROS_Project/wanda/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("test_nodes")
---
+++ wanda/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/vision/project/RBE521_ROS_Project/wanda/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/vision/project/RBE521_ROS_Project/tutorials/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/vision/project/RBE521_ROS_Project/tutorials/catkin_ws/devel;/opt/ros/noetic' != '' else []
