eac02a456d6ead83d5dc15a1ec91ac82dda8e5c4
9Volts2Ground <s.adam.stringham@gmail.com>
2021-07-03 13:56:16 -0700

    Cleared /wanda workspace

--- wanda/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vision/project/RBE521_ROS_Project/wanda/catkin_ws/src
-# Build directory: /home/vision/project/RBE521_ROS_Project/wanda/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("test_nodes")
