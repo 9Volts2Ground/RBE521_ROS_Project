c88e4ffc1c47328e1b904cf0e9d54cff6c4772d8
9Volts2Ground <s.adam.stringham@gmail.com>
2021-07-10 17:38:14 -0700

    Started Python unit testing script. Will want to expand custom Python hardware control functoins

---
+++ python/unit_testing/motor_test.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import constants
+
+# Need to figure out how to add Servo.py to path
+# Or better yet, just copy over the code and make a fresh script
+#from Servo import *
+
+def motor_test():
+
