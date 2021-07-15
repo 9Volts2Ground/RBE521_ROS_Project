81a01e8b711bbc613647141708caadfd94882498
9Volts2Ground <s.adam.stringham@gmail.com>
2021-07-14 19:11:39 -0700

    Added arguments to motor_test.py

--- python/unit_testing/motor_test.py
+++ python/unit_testing/motor_test.py
@@ -1,9 +1,12 @@
+import argparse
 import numpy as np
 import sys
 
+# Custom constants script
 sys.path.append( '..' )
 import constants
 
+# Code from Freenove robot kit
 sys.path.insert( 1, '../freenove' )
 from Servo import *
 
@@ -11,13 +14,38 @@ from Servo import *
 # Or better yet, just copy over the code and make a fresh script
 #from Servo import *
 
-def motor_test():
+def motor_test(leg, motor, angle):
     
     hrd = constants.hardware()
     servo = Servo()
 
-    servo.setServoAngle(hrd.legChannel[0,2], 0.0)
+
+
+    servo.setServoAngle( hrd.legChannel[leg,motor], angle )
 
 
 if __name__ == "__main__":
-    motor_test()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--angle', help='Angle for motor to move' )
+    parser.add_argument('-l', '--leg', help='Leg number to move (0-5)')
+    parser.add_argument('-m', '--motor', help='Motor to move (0,1,2)')
+
+    args = parser.parse_args()
+
+    if args.angle:
+        angle = args.angle
+    else:
+        angle = 0.0
+
+    if args.leg:
+        leg = args.leg
+    else:
+        leg = 0
+
+    if args.motor:
+        motor = args.motor
+    else:
+        motor = 0
+
+    motor_test(leg, motor, angle)
