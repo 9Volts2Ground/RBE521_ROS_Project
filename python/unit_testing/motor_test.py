3555dcfb01feeb1f06c5a7585f3c618982b58013
9Volts2Ground <s.adam.stringham@gmail.com>
2021-07-14 19:04:56 -0700

    Started making motor testing Python script

--- python/unit_testing/motor_test.py
+++ python/unit_testing/motor_test.py
@@ -1,9 +1,23 @@
-import numpy as np 
+import numpy as np
+import sys
+
+sys.path.append( '..' )
 import constants
+
+sys.path.insert( 1, '../freenove' )
+from Servo import *
 
 # Need to figure out how to add Servo.py to path
 # Or better yet, just copy over the code and make a fresh script
 #from Servo import *
 
 def motor_test():
+    
+    hrd = constants.hardware()
+    servo = Servo()
 
+    servo.setServoAngle(hrd.legChannel[0,2], 0.0)
+
+
+if __name__ == "__main__":
+    motor_test()
