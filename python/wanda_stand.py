8a0d120f55e42e681861903daa551732ff7ee816
9Volts2Ground <s.adam.stringham@gmail.com>
2021-07-24 13:36:58 -0700

    Starting to get her moving... but not properly yet

--- python/wanda_stand.py
+++ python/wanda_stand.py
@@ -4,6 +4,7 @@ import sys
 from ikine_hexapod import ikine_hexapod
 from ikine2angles import ikine2angles
 import constants
+import time
 
 sys.path.insert( 1, "../unit_testing")
 from move_motor import move_motor
@@ -13,15 +14,18 @@ from Servo import *
 
 
 # Set this flag if testing in Wanda or Vision
-# Wanda = True
-Wanda = False
+Wanda = True
+# Wanda = False
+
+if Wanda:
+    servo = Servo()
 
 
 hrd = constants.hardware()
 Rf = hrd.Rf
 Alph = hrd.alpha
 
-P = np.array([0.0, 0.0, 30.0, 0.0, 0.0, 0.0])
+P = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
 u = np.transpose( np.array( [ [ -Rf*np.sin(Alph/2.0), Rf*np.cos(Alph/2.0), 0.0 ],
                               [ Rf*np.sin(Alph/2.0), Rf*np.cos(Alph/2.0), 0.0 ],
@@ -31,26 +35,42 @@ u = np.transpose( np.array( [ [ -Rf*np.sin(Alph/2.0), Rf*np.cos(Alph/2.0), 0.0 ]
                               [ Rf*np.sin(5.0*Alph/2.0), Rf*np.cos(5.0*Alph/2.0), 0.0 ] ] ) )
 
 
-# Calculate overall leg vectors
-Leng, n, R = ikine_hexapod( P, u )
+for z in range(100):
 
-# Find individual servo angles
-alpha, beta, gamma = ikine2angles( P, Leng, n, R, u )
+    P[2] = z
 
 
+    # Calculate overall leg vectors
+    Leng, n, R = ikine_hexapod( P, u )
 
-if Wanda:
+    # Find individual servo angles
+    alpha, beta, gamma = ikine2angles( P, Leng, n, R, u )
 
-    servo = Servo()
+    print("================================")
 
-    for leg, alph in enumerate( alpha ):
-        move_motor( servo, leg, 0, alph )
+    print(P)
 
-    for leg, bet in enumerate( beta ):
-        move_motor( servo, leg, 1, bet )
+    print("alpha: " + str(alpha[0]) + str(alpha[1]) + str(alpha[2]) + str(alpha[3]) + str(alpha[4]) + str(alpha[5]) )
 
-    for leg, gam in enumerate( gamma ):
-        move_motor( servo, leg, 2, gam )
+    print("beta: " + str(beta[0]) + str(beta[1]) + str(beta[2]) + str(beta[3]) + str(beta[4]) + str(beta[5]) )
+
+    print("gamma: " + str(gamma[0]) + str(gamma[1]) + str(gamma[2]) + str(gamma[3]) + str(gamma[4]) + str(gamma[5]) )
+
+
+
+
+    if Wanda:
+
+        for leg, alph in enumerate( alpha ):
+            move_motor( servo, leg, 0, alph )
+
+        for leg, bet in enumerate( beta ):
+            move_motor( servo, leg, 1, bet )
+
+        for leg, gam in enumerate( gamma ):
+            move_motor( servo, leg, 2, gam )
+
+        time.sleep( .20  )
 
 
 
